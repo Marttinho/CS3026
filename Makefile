76fd232a6faed69a4d81e5b5c65d9535892cb541
Marttinho <martin.nahalka.14@aberdeen.ac.uk>
2016-11-21 02:12:17 +0000

    CS 3026 Task1 finished

---
+++ Makefile
@@ -0,0 +1,2 @@
+hellomake: shell.c filesys.c
+	gcc -o shell shell.c filesys.c -I.
---
+++ shell.c
@@ -0,0 +1,11 @@
+#include "filesys.h"
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+
+void main()
+{
+	format("CS3026 Operating Systems Assesment");
+	writedisk("virtualdiskD3_D1");
+}
